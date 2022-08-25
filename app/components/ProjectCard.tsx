03df0d3b804d261e3f90096caeecb3b61ca90893
Ashton Sobeck <asobeck@g.clemson.edu>
2022-08-24 22:40:04 -0400

    init work on projects section

---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,20 @@
+export type ProjectCardProps = {
+  projectName: string;
+  projectDescription: string;
+  projectTech: string;
+  projectLink: URL;
+};
+
+export const ProjectCard = (props: ProjectCardProps) => {
+  return (
+    <div
+      className="rounded-xl bg-white"
+    >
+      <div
+        className="md:flex"
+      >
+        
+      </div>
+    </div>
+  );
+}
---
+++ app/components/Projects.tsx
@@ -0,0 +1,9 @@
+export const Projects = () => {
+  return (
+    <section>
+      <h2>
+        A Few Projects That I've Worked On!
+      </h2>
+    </section>
+  );
+}
