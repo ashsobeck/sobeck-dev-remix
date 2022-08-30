fd5a307a990b09536ea0e17f6349c059f567b084
Ashton Sobeck <asobeck@g.clemson.edu>
2022-08-29 21:01:06 -0400

    progress on project card - very rough

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,7 +2,31 @@ import { ProjectCard } from "./ProjectCard"
 
 export const Projects = () => {
   return (
-    <section>
+    <section className="flex-col max-w-full justify-around p-1 mx-auto">
+      <ProjectCard
+        projectName="Melbourne Pedestrian Density"
+        projectDescription="hello"
+        projectTech="javascript, d3"
+        projectLink={"https://ashsobeck.github.io/melbourne-pedestrian-visualization/"}
+      />
+      <ProjectCard
+        projectName="Melbourne Pedestrian Density"
+        projectDescription="hello"
+        projectTech="javascript, d3"
+        projectLink={"https://ashsobeck.github.io/melbourne-pedestrian-visualization/"}
+      />
+      <ProjectCard
+        projectName="Melbourne Pedestrian Density"
+        projectDescription="hello"
+        projectTech="javascript, d3"
+        projectLink={"https://ashsobeck.github.io/melbourne-pedestrian-visualization/"}
+      />
+      <ProjectCard
+        projectName="Melbourne Pedestrian Density"
+        projectDescription="hello"
+        projectTech="javascript, d3"
+        projectLink={"https://ashsobeck.github.io/melbourne-pedestrian-visualization/"}
+      />
       <ProjectCard
         projectName="Melbourne Pedestrian Density"
         projectDescription="hello"
